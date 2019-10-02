ad9fbecae71c702591ff2c776da7a32502cb0445
Alberto <info@androiddesdecero.com>
2019-10-02 21:01:54 +0200

    2

--- src/test/java/junit5/CalculatorTest.java
+++ src/test/java/junit5/CalculatorTest.java
@@ -1,5 +1,6 @@
 package junit5;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     private Calculator calculator;
+    private Calculator calculatorNull;
 
     /*
      //METODO////////////////////////////////////////ESPECIFICACIOM//////////////////////////////////////////////////////////////////////////////
@@ -36,10 +38,22 @@ class CalculatorTest {
         System.out.println("@BeforeEach -> setUp()");
     }
 
+    @AfterEach
+    public void tearDown(){
+        calculator = null;
+        System.out.println("@AfterEach -> tearDown()");
+    }
+
     @Test
     public void calculatorNotNullTest(){
         assertNotNull(calculator, "Calculator debe ser not null");
         System.out.println("@Test -> calculatorNotNullTest()");
+    }
+
+    @Test
+    public void calculatorNullTest(){
+        assertNull(calculatorNull);
+        System.out.println("@Test -> calculatorNullTest()");
     }
 
 
