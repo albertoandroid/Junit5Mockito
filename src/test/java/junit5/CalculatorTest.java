40025d72564f3e27428fafba3f28ccdcb127bfdb
Alberto <info@androiddesdecero.com>
2019-10-06 18:56:29 +0200

    afterAll

--- src/test/java/junit5/CalculatorTest.java
+++ src/test/java/junit5/CalculatorTest.java
@@ -1,8 +1,6 @@
 package junit5;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +11,7 @@ class CalculatorTest {
 
     private Calculator calculator;
     private Calculator calculatorNull;
+    private static Calculator calculatorStatic;
 
     /*
      //METODO////////////////////////////////////////ESPECIFICACIOM//////////////////////////////////////////////////////////////////////////////
@@ -32,6 +31,12 @@ class CalculatorTest {
     restar(int a, int b)            |a = 10, b=20       |-10
      */
 
+    @BeforeAll
+    public static void beforeAllTests(){
+        calculatorStatic = new Calculator();
+        System.out.println("@BeforeAll -> beforeAllTests()");
+    }
+
     @BeforeEach
     public void setUp(){
         calculator = new Calculator();
@@ -44,8 +49,15 @@ class CalculatorTest {
         System.out.println("@AfterEach -> tearDown()");
     }
 
+    @AfterAll
+    public static void afterAllTests(){
+        calculatorStatic = null;
+        System.out.println("@AfterAll -> afterAllTests()");
+    }
+
     @Test
     public void calculatorNotNullTest(){
+        assertNotNull(calculatorStatic, "Calculator debe ser not null");
         assertNotNull(calculator, "Calculator debe ser not null");
         System.out.println("@Test -> calculatorNotNullTest()");
     }
@@ -132,6 +144,45 @@ class CalculatorTest {
     @Test
     public void divide_InValidInput_ExpectedException_Test(){
         assertThrows(ArithmeticException.class, ()->calculator.divideByZero(2,0), "No se puede dividir por cero");
+    }
+
+    @Disabled("Disabled until bug 23 be resolved")
+    @Test
+    public void divide_InvalidInput_Test(){
+        assertEquals(2, calculator.divide(5,0));
+    }
+
+    @Test
+    @DisplayName("Metodo Dividir -> Funciona")
+    public void divide_ValidInput_ValidResultExpected_Name_Test(){
+        assertEquals(2, calculator.divide(10,5));
+    }
+
+    @Test
+    public void add_Assert_All_Test(){
+        assertAll(
+                ()-> assertEquals(31, calculator.add(11,20)),
+                ()-> assertEquals(20, calculator.add(10,20)),
+                ()-> assertEquals(2, calculator.add(1,1))
+        );
+    }
+
+    @Nested
+    class AddTest{
+        @Test
+        public void add_Positive_Test(){
+            assertEquals(30, calculator.add(15,15));
+        }
+
+        @Test
+        public void add_Negative_Test(){
+            assertEquals(-30, calculator.add(-15,-15));
+        }
+
+        @Test
+        public void add_Zero_Test(){
+            assertEquals(0, calculator.add(0,0));
+        }
     }
 
 
