65ea9d9e3e294247a4d1f493d5d0e04154753932
Alberto <info@androiddesdecero.com>
2019-10-09 06:29:26 +0200

    2

---
+++ src/test/java/mockito/AddTest.java
@@ -0,0 +1,35 @@
+package mockito;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class AddTest {
+
+    @InjectMocks
+    private Add add;
+    @Mock
+    private ValidNumber validNumber;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void addTest(){
+        when(validNumber.check(3)).thenReturn(true);
+        boolean checkNumber = validNumber.check(3);
+        assertEquals(true, checkNumber);
+
+        when(validNumber.check("a")).thenReturn(false);
+        checkNumber = validNumber.check("a");
+        assertEquals(false, checkNumber);
+    }
+
+}
