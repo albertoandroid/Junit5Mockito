ba10961367512d3246a46ed6e76826ffa336b99c
Alberto <info@androiddesdecero.com>
2019-10-12 10:16:05 +0200

    2

---
+++ src/test/java/mockito/LoginTest.java
@@ -0,0 +1,69 @@
+package mockito;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class LoginTest {
+
+    @InjectMocks
+    private Login login;
+
+    @Mock
+    private WebService webService;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void doLoginTest(){
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                String user = (String)invocationOnMock.getArguments()[0];
+                assertEquals("Alberto", user);
+                String password = (String)invocationOnMock.getArguments()[1];
+                assertEquals("12345", password);
+                Callback callback = (Callback)invocationOnMock.getArguments()[2];
+                callback.onSuccess("OK");
+                return null;
+            }
+        }).when(webService).login(anyString(), anyString(), any(Callback.class));
+
+        login.doLogin();
+        verify(webService, times(1)).login(anyString(), anyString(), any(Callback.class));
+        assertEquals(login.isLogin, true);
+    }
+
+    @Test
+    public void doLoginErrorTest(){
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                String user = (String)invocationOnMock.getArguments()[0];
+                assertEquals("Alberto", user);
+                String password = (String)invocationOnMock.getArguments()[1];
+                assertEquals("12345", password);
+                Callback callback = (Callback)invocationOnMock.getArguments()[2];
+                callback.onFail("Error");
+                return null;
+            }
+        }).when(webService).login(anyString(), anyString(), any(Callback.class));
+
+        login.doLogin();
+        verify(webService, times(1)).login(anyString(), anyString(), any(Callback.class));
+        assertEquals(login.isLogin, false);
+    }
+
+}
